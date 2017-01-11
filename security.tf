e33c2a4ffed8ffe6a3e4d9c428efea189e2c803f
Rohith <gambol99@gmail.com>
2017-01-11 17:05:43 +0000

    Security Rules
    - adding the security group rules from compute into masters

--- security.tf
+++ security.tf
@@ -46,6 +46,26 @@ resource "aws_security_group_rule" "secure_permit_6443" {
   source_security_group_id = "${var.compute_sg}"
 }
 
+## Permit Kubernetes Exec client from secure api
+resource "aws_security_group_rule" "compute_permit_10250" {
+  type                     = "ingress"
+  security_group_id        = "${var.compute_sg}"
+  protocol                 = "tcp"
+  from_port                = "10250"
+  to_port                  = "10250"
+  source_security_group_id = "${var.secure_sg}"
+}
+
+## Permit all inbound connections from the ELB layer to service ports
+resource "aws_security_group_rule" "compute_node_ports_32000" {
+  type                     = "ingress"
+  security_group_id        = "${var.compute_sg}"
+  protocol                 = "tcp"
+  from_port                = "30000"
+  to_port                  = "32767"
+  source_security_group_id = "${var.elb_sg}"
+}
+
 #
 ## Secure ELB Rules
 #
